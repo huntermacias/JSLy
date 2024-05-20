a04500183d3bf90392e1daf84be1d38ce96699d2
supercodersf@gmail.com <huntermacias@Hunters-MacBook-Pro.local>
2024-05-19 23:29:48 -0700

    docs added

--- app.config.ts
+++ app.config.ts
@@ -1,11 +1,11 @@
 // https://github.com/nuxt-themes/docus/blob/main/nuxt.schema.ts
 export default defineAppConfig({
   docus: {
-    title: 'Docus',
-    description: 'The best place to start your documentation.',
+    title: 'JSLy',
+    description: 'The best place to master JavaScript',
     image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
     socials: {
-      twitter: 'nuxt_js',
+      twitter: 'huntermacias_',
       github: 'nuxt-themes/docus',
       nuxt: {
         label: 'Nuxt',
