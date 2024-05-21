2c8898c319fa09afc7aed8791f84464b1248e36b
supercodersf@gmail.com <huntermacias@Hunters-MacBook-Pro.local>
2024-05-20 21:48:17 -0700

    updated header

--- app.config.ts
+++ app.config.ts
@@ -3,7 +3,7 @@ export default defineAppConfig({
   docus: {
     title: 'JSLy',
     description: 'The best place to master JavaScript',
-    image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
+    image: 'https://img.freepik.com/free-vector/matrix-style-binary-code-digital-background-with-falling-numbers_1017-25336.jpg?t=st=1714430629~exp=1714434229~hmac=ca3d9c37e72cb7532000819e7fda366391043f69fbedb5695a0ba1ec8dda957b',
     socials: {
       twitter: 'huntermacias_',
       github: 'nuxt-themes/docus',
